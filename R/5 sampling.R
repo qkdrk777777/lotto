35d52467c9819ac94da7cb0d6523074dae121d50
qkdrk777777 <qkdrk777777@naver.com>
2018-01-31 04:35:49 +0900

    update

--- R/5 sampling.R
+++ R/5 sampling.R
@@ -6,8 +6,9 @@
 #' lottosample(n=5,keep=k[1])
 #' @return
 #' @export
-lottosample<-function(n,keep=NULL,drop=NULL,a=list()){
-for(i in 1:n){a[[i]]<-sort(sample(1:45,6,prob=del(keep=keep,drop=drop)))
-print(a[[i]])}
+lottosample<-function(n,keep=NULL,drop=NULL,a=NULL){
+for(i in 1:n){b<-sort(sample(1:45,6,prob=del(keep=keep,drop=drop)))
+a<-rbind(a,b)
+print(b)}
   return(a)}
 

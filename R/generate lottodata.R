5a526ee7ef33b0357f984afe11467238c8531fc9
qkdrk777777 <qkdrk777777@naver.com>
2018-02-04 02:12:31 +0900

    update

--- R/generate lottodata.R
+++ R/generate lottodata.R
@@ -6,7 +6,11 @@
 #' @examples
 #' a='https://search.naver.com/search.naver?sm=tab_drt&where=nexearch&query='
 #' b='%ED%9A%8C%EB%A1%9C%EB%98%90'
-#' numdata(a,b,last=789)
+#' url<-'https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%A1%9C%EB%98%90&oquery=%EB%A1%9C%EB%98%90&tqi=TmrKidpVuFdsssc0EvVssssssUd-075322'
+#'line<-read_html(url,encoding="UTF-8")
+#'p1<-html_nodes(line,css='._lotto-btn-current em')%>%html_text()
+#'last=as.numeric(substr(p1,1,3))
+#' numdata(a,b,last=last)
 #' @export
 numdata<-function(a,b,first=1,last,css='.num_box',encoding='UTF-8',num=list(),output=list(),k=1)
 { package('XML');  package('stringr');  package('rvest')
@@ -23,5 +27,6 @@ numdata<-function(a,b,first=1,last,css='.num_box',encoding='UTF-8',num=list(),ou
   colnames(output)=c('x1','x2','x3','x4','x5','x6','times')
   rownames(output)=output[,7]
   return(output)}
-#numdata(a,b,first=790,last=791)
+#numdata(a,b,first=790,last=792)
+
 
